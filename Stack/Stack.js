4a0d0c9d0d65873731bcef71b902f4b34743b577
Amrendra Nath <amrendra@Amrendras-MacBook-Air.local>
2019-11-14 22:54:06 +0530

    implement stack using linked list

---
+++ Stack/Stack.js
@@ -0,0 +1,44 @@
+import LinkedList from "../LinkedList/LinkedList";
+
+export default class Stack {
+    constructor () {
+        this.linkedList = new LinkedList();
+    }
+
+    /**
+     * check if stack is empty or not
+     * @return {boolean} 
+     */
+    isEmpty() {
+        return !this.linkedList.head;
+    }
+
+    /**
+     * Get the value from top of the stack
+     * @return {*}
+     */
+    peek() {
+        if (this.isEmpty()) {
+            return null;
+        }
+
+        return this.linkedList.head.value;
+    }
+
+    /**
+     * add new element at the top of the stack (the head)
+     * @param {*} value 
+     */
+    push(value) {
+        this.linkedList.prepend(value);
+    }
+
+    /**
+     * remove the element from the top of the stack (the head)
+     * if top (the head) is empty return null.
+     */
+    pop() {
+        const removeHead = this.linkedList.deleteHead();
+        return removeHead ? removeHead.value : null;
+    }
+}
